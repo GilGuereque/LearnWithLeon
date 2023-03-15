860f52ad1172824cf3c58ab5c7c579875e0f4a71
GilGuereque <gguereque326@gmail.com>
2023-03-14 20:52:11 -0700

    Updated objects-child-classes main.js

--- Javascript/class31-materials/objects-child-class/js/main.js
+++ Javascript/class31-materials/objects-child-class/js/main.js
@@ -6,13 +6,58 @@ class Car {
     accellerate(){
         console.log("vrooom vroooom mathhaaafuckkkaa!!");
     }
-}
+};
 
 class Toyota extends Car{
     constructor(make,model){
         super(make);
         this.model = model
     }
-}
+};
 
 let supra = new Toyota('Toyota','Supra');
+
+//Leon's solution
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`);
+    }
+};
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name);
+        this.breed = breed;
+    } 
+};
+
+let simba = new Dog('Simba', 'Sheperd');
+
+class Cat extends Animal{
+    constructor(name, breed){
+        super(name);
+        this.breed = breed;
+    }
+    speak(){
+        console.log('meow');
+    }
+};
+//create cat
+let fido = new Cat('Fido','House Cat');
+
+//my addition to Class Animal
+
+
+class Whale extends Animal{
+    constructor(name, breed){
+        super(name);
+        this.breed = breed;
+    }
+    speak(){
+        console.log('buuuuuuuuhhhhhhhhhhhhhhhhhh');
+    }
+};
+
+let willy = new Whale('Willy','Killer Whale');
